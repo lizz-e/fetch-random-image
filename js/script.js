a8be2fc52b9631c946211a605167aebc16be5601
lizzette_e <lizz.enorme@gmail.com>
2024-05-24 19:45:41 -0500

    created function to randomly select an image and created event listener to display the image

--- js/script.js
+++ js/script.js
@@ -7,6 +7,31 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images); 
+    // console.log(images); 
+
+    selectRandomImage(images);
 };
-getImage();
+
+// randomly select index of an image
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    // console.log(randomIndex);
+    // console.log(randomImage);
+
+    displayImage(randomImage);
+};
+
+// display the image
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress
+    imgDiv.classList.remove("hide");
+};
+
+// add click event
+button.addEventListener ("click", function () {
+    getImage();
+});
